c4dad78e090d2664f0c2c98107bdae8419bf2641
unknown <eka@rudito.web.id>
2013-09-23 06:14:45 +0700

    modification inputTindakan

--- SIM-RS/RAWAT-JALAN-PAV/inputTindakan.Designer.cs
+++ SIM-RS/RAWAT-JALAN-PAV/inputTindakan.Designer.cs
@@ -64,6 +64,7 @@
             this.btnKeluarIsiTindakan = new System.Windows.Forms.Button();
             this.btnSimpanIsiTindakan = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.txtKeringananTotal = new System.Windows.Forms.TextBox();
             this.lblTerbilang = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
@@ -73,11 +74,12 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.txtKeringananItem = new System.Windows.Forms.TextBox();
-            this.txtKeringananTotal = new System.Windows.Forms.TextBox();
             this.panel5 = new System.Windows.Forms.Panel();
             this.label9 = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
             this.label14 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -482,6 +484,8 @@
             // 
             this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(241)))));
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.checkBox1);
             this.panel4.Controls.Add(this.txtKeringananTotal);
             this.panel4.Controls.Add(this.lblTerbilang);
             this.panel4.Controls.Add(this.label12);
@@ -495,13 +499,21 @@
             this.panel4.Size = new System.Drawing.Size(396, 566);
             this.panel4.TabIndex = 141;
             // 
+            // txtKeringananTotal
+            // 
+            this.txtKeringananTotal.BackColor = System.Drawing.Color.White;
+            this.txtKeringananTotal.Location = new System.Drawing.Point(176, 78);
+            this.txtKeringananTotal.Name = "txtKeringananTotal";
+            this.txtKeringananTotal.Size = new System.Drawing.Size(152, 27);
+            this.txtKeringananTotal.TabIndex = 144;
+            // 
             // lblTerbilang
             // 
             this.lblTerbilang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(241)))));
             this.lblTerbilang.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTerbilang.Location = new System.Drawing.Point(15, 247);
+            this.lblTerbilang.Location = new System.Drawing.Point(14, 433);
             this.lblTerbilang.Name = "lblTerbilang";
-            this.lblTerbilang.Size = new System.Drawing.Size(365, 302);
+            this.lblTerbilang.Size = new System.Drawing.Size(365, 114);
             this.lblTerbilang.TabIndex = 125;
             this.lblTerbilang.Text = "SERATUS LIMA JUTA RUPIAH";
             // 
@@ -510,7 +522,7 @@
             this.label12.AutoSize = true;
             this.label12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(241)))));
             this.label12.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(14, 152);
+            this.label12.Location = new System.Drawing.Point(14, 329);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(337, 25);
             this.label12.TabIndex = 124;
@@ -522,7 +534,7 @@
             this.label11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(241)))));
             this.label11.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.Color.Black;
-            this.label11.Location = new System.Drawing.Point(11, 187);
+            this.label11.Location = new System.Drawing.Point(11, 364);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(200, 45);
             this.label11.TabIndex = 123;
@@ -595,14 +607,6 @@
             this.txtKeringananItem.Size = new System.Drawing.Size(184, 27);
             this.txtKeringananItem.TabIndex = 143;
             // 
-            // txtKeringananTotal
-            // 
-            this.txtKeringananTotal.BackColor = System.Drawing.Color.White;
-            this.txtKeringananTotal.Location = new System.Drawing.Point(176, 78);
-            this.txtKeringananTotal.Name = "txtKeringananTotal";
-            this.txtKeringananTotal.Size = new System.Drawing.Size(152, 27);
-            this.txtKeringananTotal.TabIndex = 144;
-            // 
             // panel5
             // 
             this.panel5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
@@ -669,12 +673,31 @@
             this.label14.Text = "&DATA SOSIAL PASIEN";
             this.label14.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(24, 118);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 29);
+            this.checkBox1.TabIndex = 145;
+            this.checkBox1.Text = "ASURANSI : ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(176, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 27);
+            this.textBox1.TabIndex = 146;
+            // 
             // inputTindakan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(219)))), ((int)(((byte)(219)))));
-            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.ClientSize = new System.Drawing.Size(1024, 750);
             this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
@@ -760,5 +783,7 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
