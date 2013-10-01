b1e5863b5ccf0bf8c4d778359aaa62e57305f14a
Eka Rudito <eka@rudito.web.id>
2013-10-01 15:44:50 +0700

    remove unused text

--- SIM-RS/Program.cs
+++ SIM-RS/Program.cs
@@ -5,22 +5,17 @@ using System.Windows.Forms;
 
 namespace SIM_RS
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new halamanUtama());
-
-            //ppppp
-
-			//test putu
-
-        }
-    }
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new halamanUtama());
+		}
+	}
 }
